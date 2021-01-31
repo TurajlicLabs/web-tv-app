226484ec77c2e748159d1f9e1dd1b1bbeafc1e86
Dusan Turajlic <dusan@turajlic.fi>
2021-01-31 19:47:26 +0200

    Creates loading and fetches channels

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="text-white bg-blue-700 inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md bg-rose-600">
+    <Loading />
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Loading from './Loading'
+export default defineComponent({
+  name: 'Button',
+  components: { Loading }
+})
+</script>
+
+<style scoped>
+
+</style>
